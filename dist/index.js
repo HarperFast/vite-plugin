2528630b0532ec7a40971a4ec3c04ca31ec771c9
Austin Akers <austin@harperdb.io>
2025-12-12 15:37:33 -0500

    updating package with .gitignore

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,7 @@
 import { join } from "node:path";
 import * as vite from 'vite';
 export async function handleApplication(scope) {
-    //   return serve(scope.directory, scope.server);
-    // }
-    // async function serve(componentPath: string, server: Scope["server"]) {
     const componentPath = scope.directory;
-    // const vite = (await import(`${pathToFileURL(componentRequire.resolve('vite'))}`)).default;
     const viteInstance = await vite.createServer({
         root: componentPath,
         configFile: join(componentPath, 'vite.config.js'),
